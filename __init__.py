56a6f7070f4fd07ec2ddaa742f2a1f4b8af22f3f
Jerome Esteve <jerome.esteve@u-psud.fr>
2015-03-29 15:39:55 +0200

    Added Sphinx documentation.

--- __init__.py
+++ __init__.py
@@ -1,18 +1,10 @@
-"""Pyslave magic functions :
+"""Pyslave contains :
 
-* Instruments functions
-openall : load all GPIB instruments
-listall : list all loaded instruments
+* Instruments functions to load VISA instruments
 
-* Script functions
-call : load script file and launch the script
-pause : pause the script
-resume : resumes the script
-abort : abort the script
-window : show the slave window
+* Instrument drivers
 
-* Miscelleanous functions 
-today : change directory to today's data directory, create it if necessary
-lastday : change directory to the directory corresponding to the last day of data
+* Magic IPython functions to run python scripts in slave
 
+* Helper functions to write scripts for slave
 """
