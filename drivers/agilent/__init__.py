ca363f850d5bb0f4157ab870720ae4f3ce8cdfe0
Jerome Esteve <jerome.esteve@u-psud.fr>
2015-06-15 16:39:07 +0200

    Easy call to dmm.

--- drivers/agilent/__init__.py
+++ drivers/agilent/__init__.py
@@ -1,3 +1,7 @@
 """Agilent devices are suppported via their IVI drivers. See the Python IVI documentation."""
 from ivi.agilent import agilent34401A
-from ivi.agilent import agilentE3641A
+from ivi.agilent import agilentE3641A as __agilentE3641A__
+
+class agilentE3641A(__agilentE3641A__):
+    def __call__(self, max_time=1):
+        return dmm1.measurement.read(max_time)
