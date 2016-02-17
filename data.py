28735f6ac588f5fda6b1b7f1805b7485cd60db15
Jerome Esteve <jerome.esteve@u-psud.fr>
2016-02-17 22:46:15 +0100

    Improved data methods.

--- data.py
+++ data.py
@@ -7,6 +7,9 @@ logger = logging.getLogger('pyslave.data')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.NullHandler())
 
+class DataException(Exception):
+    pass
+
 def __increment__(base, ext, previous, ndigits):
     rec = re.compile(base+'[0-9]*'+ext)
     index = [p[len(base):] for p in previous if rec.match(p)]
@@ -36,8 +39,14 @@ class data(dict):
     as attributes in the HDF5 format.
 
     Data object can be created by the makedata function."""
-    __data_attributes__ = []
-    __hidden_attributes__ = []
+    def __init__(self, *args, **kwargs):
+        super(dict).__init__(self, *args, **kwargs)
+        self.set_data_attributes()
+        self.set_hidden_attributes()
+    def set_data_attributes(self):
+        self.__data_attributes__ = []
+    def set_hidden_attributes(self):
+        self.__hidden_attributes__ = []
     def __getattr__(self, name):
         return self[name]
     def __setattr__(self, name, value):
@@ -46,11 +55,32 @@ class data(dict):
         del self[name]
     def plot(self, ax, **kwargs):
         __data_attributes__ = self.__data_attributes__
-        x = self[__data_attributes__[0]]
-        y = self[__data_attributes__[1]]
-        ax.plot(x, y, **kwargs)
-        ax.set_xlabel(__data_attributes__[0])
-        ax.set_ylabel(__data_attributes__[1])
+        try:
+            l = len(ax)
+        except:
+            l = 0
+        if l and l==len(__data_attributes__)-1:
+            for i,a in enumerate(ax):
+                a.cla()
+                x = self[__data_attributes__[0]]
+                y = self[__data_attributes__[i]]
+                a.plot(x, y, **kwargs)
+                a.set_xlabel(__data_attributes__[0])
+                a.set_ylabel(__data_attributes__[i])
+        else:
+            a = ax[0] if l else ax
+            for i in range(1, len(__data_attributes__)):
+                x = self[__data_attributes__[0]]
+                y = self[__data_attributes__[i]]
+                a.plot(x, y, **kwargs)
+                a.set_xlabel(__data_attributes__[0])
+                a.set_ylabel(__data_attributes__[i])
+    def append(self, *args):
+        if len(args)!=len(self.__data_attributes__):
+            raise DataException('Number of arguments does not match number of data fields : ' + ' '.join(self.__data_attributes__))
+        for i,v in enumerate(args):
+            k = self.__data_attributes__[i]
+            self[k] = np.insert(self[k], len(self[k]), v)
     @property
     def __data__(self):
         return np.core.rec.fromarrays( [self[k] for k in self.__data_attributes__] , names = self.__data_attributes__)
@@ -81,13 +111,22 @@ class data(dict):
             self.save_h5(file, **kwargs)
         else :
             raise TypeError('File should be a string or an opened HDF file.')
-    def save_txt(self, filename, increment=True, ndigits=4):
-        """Save the data to a text file. If increment is True, the filename is automatically incremented and will contain a ndigits integer."""
+    def save_txt(self, filename, attrs=dict(), increment=True, ndigits=4):
+        """Save the data to a text file.
+        If increment is True, the filename is automatically incremented and will contain a ndigits integer.
+        The non data attributes are saved in a companion text file together with the extra attributes passed in attrs.
+        """
         if increment : filename = increment_file(filename, ndigits)
         np.savetxt(filename, self.__data__)
         msg = 'Data saved to {0}.'.format(str(filename))
         logger.info(msg)
         print msg
+        attrs.update(self.__attributes__)
+        if attrs:
+            with open(filename[:-4]+'_attrs.txt', 'w') as f:
+                for k,v in attrs.iteritems():
+                    print >>f,k,v
+
     def save_h5(self, hdf, dataset='data', attrs=dict(), increment=True, ndigits=4, **kwargs):
         """Save the data to a HDF5 dataset. The first parameter hdf must a HDF5 file opened for writing.
         If increment is True, the name of the dataset is automatically incremented and will contain a ndigits integer.
@@ -112,7 +151,8 @@ class Sij(data):
     * Data attributes : freq, S12 (complex)
     * Attributes : start_frequency, stop_frequency, number_of_points, power
     """
-    __data_attributes__ = ['freq','Sij']
+    def set_data_attributes(self):
+        self.__data_attributes__ = ['freq','Sij']
     def __getitem__(self, key):
         if key is 'freq':
             return np.linspace(self.start_frequency, self.stop_frequency, self.number_of_points)
@@ -123,7 +163,7 @@ class Sij(data):
         ax.plot(self.freq/1e9, y, **kwargs)
         ax.set_xlabel('Frequency (GHz)')
         ax.set_ylabel('$|S_{ij}|^2$ (dB)' if scale is 'log' else '$|S_{ij}|^2$')
-    def save_txt(self, filename, increment=True, ndigits=4):
+    def save_txt(self, filename, attrs=dict(), increment=True, ndigits=4):
         """Save the data to a text file with three columns : freq, Sij.real, Sij.imag."""
         if increment : filename = increment_file(filename, ndigits)
         data = self.__data__
@@ -132,6 +172,11 @@ class Sij(data):
         msg = 'Data saved to {0}.'.format(str(filename))
         logger.info(msg)
         print msg
+        attrs.update(self.__attributes__)
+        if attrs:
+            with open(filename[:-4]+'_attrs.txt', 'w') as f:
+                for k,v in attrs.iteritems():
+                    print >>f,k,v
 
 class lecroy_trace(data):
     """Lecroy oscilloscope waveform data class.
@@ -139,8 +184,10 @@ class lecroy_trace(data):
     * Data attributes : horiz, vert
     * Attributes : horiz_interval, horiz_offset, sweeps_per_acq, bandwidth_limit, vertical_gain, vertical_offset, vert_coupling, acq_vert_offset, probe_att
     """
-    __data_attributes__ = ['horiz','vert']
-    __hidden_attributes__ = ['wave']
+    def set_data_attributes(self):
+        self.__data_attributes__ = ['horiz','vert']
+    def set_hidden_attributes(self):
+        self.__hidden_attributes__ = ['wave']
     def plot(self, ax, **kwargs):
         ax.plot(self.horiz, self.vert, **kwargs)
         ax.set_xlabel('Time (s)')
@@ -160,28 +207,61 @@ class lecroy_trace(data):
 
 class xy(data):
     """Generic x,y data class.
-    
+
     * Data Attributes : x, y
     """
-    __data_attributes__ = ['x', 'y']
+    def set_data_attributes(self):
+        self.__data_attributes__ = ['x', 'y']
 
 
-def makedata(*args):
-    """Create a data object (see the data class).
+def makedata(*args,**kwargs):
+    """Create a data instance (see the data class).
 
-    Arguments should come by pairs of 'name', value. For example :
-    mydata = makedata('i', i, 'v', v, 'Rcernox', Rc)
+    Arguments should be passed as to create a dictionary. For example:
+    mydata = makedata(i=i, v=v, Rcernox=Rc)
+    mydata = makedata( ('i',i), ('v',v), ('Rcernox',Rc) )
 
-    Variables with type array or list will be saved as arrays in a text or h5 file.
-    Variables of other type will be saved as h5 attributes or discarded if the data are saved in a text file.
+    Data can be saved with the save method:
+    mydata.save('mydata.txt')
+
+    Data can be plotted with the plot method:
+    fig, ax = subplots()
+    mydata.plot(ax)
     """
-    keys = []
-    values = []
-    __data_attributes__ = []
-    for i in range(0,len(args),2):
-        keys.append(args[i])
-        val = np.array(args[i+1]) if type(args[i+1]) is list else args[i+1]
-        values.append(val)
-        if type(val) is np.array: __data_attributes__.append(args[i])
-    res = data( zip(keys, values) )
-    res.__data_attributes__ = __data_attributes__
+    res = data(*args, **kwargs)
+    for k,v in res.iteritems():
+        if type(v) is list:
+            res[k] = array(v)
+            res.__data_attributes__.append(k)
+        if type(v) is np.array:
+            res.__data_attributes__.append(k)
+    return res
+
+def createdata(*args):
+    """Create an empty data instance (see the data class).
+
+    Expects string arguments that will refer to the name of the data fields. For example:
+    mydata = createdata('i', 'v', 'Rc')
+
+    Data points can be added with the append method:
+    mydata.append(i_value, v_value, Rc_value)
+
+    Parameters (non-data) fields are appended as dictionary (key,values) or as attributes.
+    mydata['lockin frequency'] = 77.0
+    madata.lockin_amplitude = 0.1
+
+    Data can be saved with the save method:
+    mydata.save('mydata.txt')
+
+    Data can be plotted with the plot method:
+    fig, ax = subplots()
+    mydata.plot(ax)
+    """
+    res = data(*args, **kwargs)
+    for k,v in res.iteritems():
+        if type(v) is list:
+            res[k] = array(v)
+            res.__data_attributes__.append(k)
+        if type(v) is np.array:
+            res.__data_attributes__.append(k)
+    return res
