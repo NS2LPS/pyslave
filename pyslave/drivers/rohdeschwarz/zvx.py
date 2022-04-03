2a69734f4d2aaa85729b7a67e46d6c2b5039af4d
Jérôme Esteve <jerome.esteve@u-psud.fr>
2022-04-03 21:18:42 +0200

    Improved zvx doc

--- pyslave/drivers/rohdeschwarz/zvx.py
+++ pyslave/drivers/rohdeschwarz/zvx.py
@@ -34,30 +34,47 @@ class zva(myrszvb.rszvb):
         self.read = self.instrument.read
         self.write('FORM:DAT REAL ,32')
     def fetch(self, channel=1, refresh_parameters=True):
-        """Fetch the trace from the specified channel and return it as a Sij data object."""
+        """Fetch the trace from the specified channel and return it as a Sij data object.
+        
+        :param channel: Channel number, defaults to 1
+        :param refresh_parameters: Refresh the VNA parameters before saving, defaults to True"""
         data = self.TraceResponseData(channel)
         if refresh_parameters : self.refresh_parameters(channel)
         return Sij( Sij = data, **self.__acquisition_parameters__ )
     def fetch_cw(self, channel=1, refresh_parameters=True):
-        """Fetch the trace from the specified channel and return it as a data object."""
+        """Fetch the trace from the specified channel and return it as a data object.
+        
+        :param channel: Channel number, defaults to 1
+        :param refresh_parameters: Refresh the VNA parameters before saving, defaults to True"""
         data = self.TraceResponseData(channel)
         if refresh_parameters : self.refresh_parameters_cw(channel)
         return Data( Sij = data, **self.__acquisition_parameters__ )        
     def fetch_raw(self, channel=1):
-        """Fetch the trace from the specified channel and return it as a complex array."""
+        """Fetch the trace from the specified channel and return it as a complex array.
+        
+        :param channel: Channel number, defaults to 1"""
         return self.TraceResponseData(channel)
     def fetch_segments(self, nsegments, npoints, channel=1):
-        """Fetch the segemented trace from the specified channel and return it as a complex array."""
+        """Fetch the segemented trace from the specified channel and return it as a complex array.
+        
+        :param nsegements
+        :param npoints
+        :param channel: Channel number, defaults to 1"""
         data = self.TraceResponseData(channel).reshape(nsegments,npoints,2)
         return data
     def wait_for_average(self, channel=1, sleep_time=0.5):
-        """Blocks until the enough traces are acquired to perform the average."""
+        """Blocks until the enough traces are acquired to perform the average.
+        
+        :param channel: Channel number, defaults to 1
+        :param sleep_time, defaults to 0.5"""
         n_average = self.GetAverageFactor(channel)
         time.sleep(sleep_time)
         while self.GetCurrentSweep(channel)<n_average :
             time.sleep(sleep_time)
     def acquisition_parameters(self, channel = 1):
-        """Return the acquisition parameters for the specified channel."""
+        """Return the acquisition parameters for the specified channel.
+        
+        :param channel: Channel number, defaults to 1"""
         start = self.GetStartFrequency(channel)
         stop = self.GetStopFrequency(channel)
         npoints = self.GetSweepNumberOfPoints(channel)
@@ -65,7 +82,9 @@ class zva(myrszvb.rszvb):
         rbw = self.GetMeasBandwidth(channel)
         return dict( start_frequency = start, stop_frequency = stop, number_of_points = npoints, power = power, rbw = rbw )
     def acquisition_parameters_cw(self, channel = 1):
-        """Return the acquisition parameters for the specified channel."""
+        """Return the acquisition parameters for the specified channel.
+
+        :param channel: Channel number, defaults to 1"""
         npoints = self.GetSweepNumberOfPoints(channel)
         power = self.GetPower(channel)
         rbw = self.GetMeasBandwidth(channel)
@@ -73,10 +92,14 @@ class zva(myrszvb.rszvb):
         freq = self.GetCWFrequency(channel)
         return dict( number_of_points = npoints, power = power, rbw=rbw, sweeptime=sweeptime, freq=freq)
     def refresh_parameters(self, channel = 1):
-        """Refresh the acquisition paramters and store them"""
+        """Refresh the acquisition paramters and store them
+        
+        :param channel: Channel number, defaults to 1"""
         self.__acquisition_parameters__ = self.acquisition_parameters(channel)
     def refresh_parameters_cw(self, channel = 1):
-        """Refresh the acquisition paramters and store them"""
+        """Refresh the acquisition paramters and store them
+        
+        :param channel: Channel number, defaults to 1"""
         self.__acquisition_parameters__ = self.acquisition_parameters_cw(channel)
     def __del__(self):
         try:
