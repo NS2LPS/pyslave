26db5256f212e310370658bd40abc038abcc3843
Jérôme Esteve <jerome.esteve@u-psud.fr>
2022-04-03 22:25:19 +0200

    Debug

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyslave'
-copyright = u'2018, Jérôme Estève'
+copyright = u'2022, Jérôme Estève'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
